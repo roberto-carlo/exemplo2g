bcfd2c3037bd952a799640e3b6e350d80a53cbff
roberto-carlo <contato.martinsjonatas@gmail.com>
2023-05-19 10:00:31 -0300

    Durante a aula compreendi sobre as variaveis o conceito da materia ()diferença entre LIP E PLAP)

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
         //aula introdutora de introduçâo a objetos
+
+        Pessoa adao  =  new Pessoa();
+
+        //declraçao de classes
+        Pessoa qualquer;
+
+        //instanciaçao de objeto
+        qualquer = new Pessoa();
+        Pessoa adulta = new Pessoa();
+        //definir forma do objeto
+        qualquer.nome = "Rita";
+        qualquer.sobrenoe = "Lee";
+        // definir comportamento do objeto
+        qualquer.falar();
+        adao.falar("baixo");
     }
 }
--- src/Pessoa.java
+++ src/Pessoa.java
@@ -1,4 +1,14 @@
-public class Pessoa {
+public class  Pessoa {
     //atributo
-    String nome
+
+    String nome;
+    String sobrenoe;
+            //metodo classes
+            public void falar(){
+                System.out.println("falar");
+            }
+            public void falar(String volume){
+            System.out.println("falei" + volume);
+            }
+
 }
